a4e59b6d12974eccba8e4ba226a4d2c01aa5c1f3
Seul <54494532+Seulwoo@users.noreply.github.com>
2020-10-22 13:57:07 +0900

    Create BJ1654.py

---
+++ Algorithm/2020-10/BJ1654.py
@@ -0,0 +1,23 @@
+import sys
+read = sys.stdin.readline
+
+K, N = map(int, read().split(' '))
+inputs = []
+for i in range(K):
+    inputs.append(int(read()))
+
+left = 0
+right = max(inputs)
+answer = 0
+while left <= right:
+    mid = int((left+right)/2)
+    cur = 0
+    for i in range(K):
+        cur += (inputs[i]//mid)
+    if cur >= N:
+        answer = max(answer, mid)
+        left = mid+1
+    else:
+        right = mid-1
+
+print(answer)
